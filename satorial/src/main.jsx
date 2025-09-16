d5d1e0ada544dc3088001c8f526614487947b006
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2025-09-16 23:36:48 +0100

    update: add vercel analytics

--- satorial/src/main.jsx
+++ satorial/src/main.jsx
@@ -4,10 +4,12 @@ import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./contexts/AuthContext"; // Ensure correct import
 import { ClientProvider } from "./contexts/ClientContext";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <ClientProvider>
+      <Analytics />
       <App />
     </ClientProvider>
   </AuthProvider>
